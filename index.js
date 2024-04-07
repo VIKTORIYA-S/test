74567b790566765ed1d03a20446bbda763f7a9bc
victoria <v.suprun1961@gmail.com>
2024-04-08 02:22:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// alert("Hello!");
+
+// const result = confirm("ВЫ учите JS?");
+// console.log(result);
+
+// const answer = +prompt("Как Вас зовут?", "Введите Ваше имя");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = "Ivan";
+// answers[1] = 123;
+// answers[2] = true;
+// console.log(answers);
+
+// answers[3] = prompt('Как Ваше имя?', '');
+// answers[4] = prompt('Как Ваша фамилия?', '');
+// answers[5] = prompt('Как Ваше отчество?', '');
+// answers[6] = prompt('Сколько Вам лет?', '');
+// console.log(answers);
+
+// console.log(typeof answers);
+
+// const user = "Victoriia";
+// alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5*2 === "10");
+
+
+
+
+
+
+
+
+
+
+
